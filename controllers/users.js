38463d2de09a84b51bc483ae7f28e90f98393c05
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-11-20 14:45:28 +0100

    11/20 Add new user Route adde/tested

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const usersRouts = require('express').Router();
 
-const users = [
+let users = [
   { name: 'John', age: 27 },
   { name: 'Jack', age: 19 },
   { name: 'Mack', age: 51 },
@@ -8,8 +8,34 @@ const users = [
   { name: 'Richard', age: 34 },
   { name: 'Andrew', age: 42 },
 ];
+
 usersRouts.get('/', (request, response) => {
-  response.json(users);
+  response.status(200).json(users);
+});
+
+usersRouts.post('/', (request, response) => {
+  const { name, age } = request.body;
+
+  if (!name || !age) {
+    return response.status(409).json({ error: 'Some input data is missing' });
+  }
+
+  //newName repeating check
+  let nameRepeatCheck = users.map((el) => el.name).includes(name);
+  if (nameRepeatCheck) {
+    return response
+      .status(409)
+      .json({ error: 'person with this name already exists' });
+  }
+
+  const newPerson = {
+    name: name,
+    age: +age,
+  };
+
+  users = users.concat(newPerson);
+
+  response.status(201).json(newPerson);
 });
 
 module.exports = usersRouts;
