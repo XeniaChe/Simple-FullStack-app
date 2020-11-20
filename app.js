4570748715a2f9e9ed8247437fc6f23a7f148749
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-11-20 17:06:50 +0100

    11/20 FE added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middelware');
 const usersRouts = require('./controllers/users');
 
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 app.use('/api/users', usersRouts);
 
